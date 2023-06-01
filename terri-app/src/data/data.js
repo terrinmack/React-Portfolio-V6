0c936e11eec849d280fe65218db96c186a39a808
Your Name <terrimack15@gmail.com>
2023-06-01 16:13:18 -0700

    edited data.js file to reflect projects

--- terri-app/src/data/data.js
+++ terri-app/src/data/data.js
@@ -1,35 +1,38 @@
-import WorkIm from '../assets/projects/workImg.jpeg'
-import RealEst from '../assets/projects/realestate.jpg'
+
+import Weather from '../images/projects/weatherDashboard.jpg'
+import EventDash from '../images/projects/eventDash.png'
+import EmpTracker from '../images/projects/employeeTracker.jpg'
+import MusicEars from '../images/projects/MusicToMyEars.png'
 
 export const data=[
     {
         id:1,
-        name:"React JS Application",
-        image:WorkIm,
-        github:"",
-        live:"",
+        name:"Weather API Application",
+        image:Weather,
+        github:"https://github.com/terrinmack/Weather-Dashboard",
+        live:"https://terrinmack.github.io/Weather-Dashboard/",
     },
     {
         id:2,
-        name:"React JS Application",
-        image:RealEst,
-        github:"",
-        live:"",
+        name:"Web API Application",
+        image:EventDash,
+        github:"https://github.com/caseyfee/EventDash",
+        live:"https://caseyfee.github.io/EventDash/",
     },
     {
         id:3,
-        name:"React JS Application",
-        image:WorkIm,
-        github:"",
+        name:"MySql Database",
+        image:EmpTracker,
+        github:"https://github.com/terrinmack/Employee-Tracker",
         live:"",
     },
     {
         id:4,
-        name:"React JS Application",
-        image:RealEst,
-        github:"",
-        live:"",
+        name:"Javascript, API, and Handlebar Application",
+        image:MusicEars,
+        github:"https://github.com/Ally27/Music-to-My-Ears",
+        live:"https://music-to-my-ears1.herokuapp.com/",
     },
-
+``
 
 ]
