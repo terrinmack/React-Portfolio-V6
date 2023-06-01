9c8473ee8297394109e42a2c4c03879873563bf7
Your Name <terrimack15@gmail.com>
2023-06-01 12:45:32 -0700

    restarting file paths AGAIN. Did not make it a multipage layout

--- terri-app/tailwind.config.js
+++ terri-app/tailwind.config.js
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx,css}",],
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx,css}",
+    "./src/*.css"
+  ],
   theme: {
     extend: {},
   },
